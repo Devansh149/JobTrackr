1f30f1e33398f4f7a2686c842d7c5809219b6306
Devansh Chandra <155964011+Devansh149@users.noreply.github.com>
2026-08-20 18:29:01 +0530

    Adjust Navbar padding for improved layout

--- JobTrackr/Frontend/src/components/Navbar.jsx
+++ JobTrackr/Frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className='w-full border-b-2 border-violet-200 flex items-center justify-between px-5 py-2 bg-black text-violet-200 '>
+        <div className='w-full border-b-2 border-violet-200 flex items-center justify-between px-8 py-2 bg-black text-violet-200 '>
             <img src="src\assets\logo.png" alt="logo" className='w-14 md:w-15 ' />
             <button className='border-2 h-fit px-3 py-1 text-sm rounded-2xl'>Log in</button>
         </div>
